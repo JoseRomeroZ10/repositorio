7809b5109daddb21a2646e7377120cebbefd50f0
JoseRomeroZ10 <joseandresromeroollarves@gmail.com>
2024-11-20 17:16:48 -0400

    add modules purchases and payment methods

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
+import { SuppliersModule } from './suppliers/suppliers.module';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceConfig } from './common/config/data.source';
+import { WarehousesModule } from './warehouses/warehouses.module';
+import { StocksModule } from './stocks/stocks.module';
+import { AuthModule } from './auth/auth.module';
+import { PurchasesModule } from './purchases/purchases.module';
+import { PaymentMethodsModule } from './payment-methods/payment-methods.module';
+
+@Module({
+  imports: [ 
+    ConfigModule.forRoot({
+      envFilePath: `.env.development`,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(DataSourceConfig),
+    ProductsModule, 
+    CategoriesModule, 
+    SuppliersModule, 
+    UsersModule, WarehousesModule, StocksModule, AuthModule, PurchasesModule, PaymentMethodsModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
